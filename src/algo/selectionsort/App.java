fbb3cb1d35e912bea8d1b0507c33851e16ba7897
Chad <chad.n.palmer@gmail.com>
2018-06-26 19:28:35 -0500

    selection sort shell added

--- src/algo/selectionsort/App.java
+++ src/algo/selectionsort/App.java
@@ -1,4 +1,21 @@
 package algo.selectionsort;
 
 public class App {
+
+    public static void main(String[] args) {
+
+        int[] myArray = selectionSort(new int[]{9, 8, 3, 13, 87, 12, 99});
+        displayArray(myArray);
+    }
+
+
+    public static int[] selectionSort(int arr[]) {
+        return arr;
+    }
+
+    public static void displayArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" [" + arr[i] + "]");
+        }
+    }
 }
