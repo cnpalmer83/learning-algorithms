38ae698a895a210fc076390d7e926fc68008468e
Chad <chad.n.palmer@gmail.com>
2018-06-27 21:26:51 -0500

    mergeSort shell added

---
+++ src/algo/mergesort/App.java
@@ -0,0 +1,18 @@
+package algo.mergesort;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] mergeSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static void displayArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" [" + arr[i] + "]");
+        }
+    }
+}
