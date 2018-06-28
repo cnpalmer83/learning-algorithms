6d19e4360c655b0bc460f20a42fddb410ebb287f
Chad <chad.n.palmer@gmail.com>
2018-06-27 20:56:05 -0500

    insertion sort completed

--- src/algo/insertionsort/App.java
+++ src/algo/insertionsort/App.java
@@ -2,11 +2,23 @@ package algo.insertionsort;
 
 public class App {
     public static void main(String[] args) {
-
+        int[] myArray = insertionSort(new int[] {7, 9, 3, 5, 11, 1, 4});
+        displayArray(myArray);
     }
 
     public static int[] insertionSort(int[] arr) {
-
+        for (int i = 0; i < arr.length; i++) {
+            // assign a variable "element" to hold data from unsorted portion that will be moved to sorted portion.
+            int element = arr[i];
+            // "j" variable points to index of last value in the sorted portion
+            int j = (i - 1);
+            while (j >= 0 && arr[j] > element) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = element;
+        }
+        return arr;
     }
 
     public static void displayArray(int arr[]) {
