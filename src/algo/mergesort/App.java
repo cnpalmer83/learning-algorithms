8b7da04dd65922e9d694d018227f2138a3213967
Chad <chad.n.palmer@gmail.com>
2018-06-28 11:02:54 -0500

    merge method defined, algorithm works

--- src/algo/mergesort/App.java
+++ src/algo/mergesort/App.java
@@ -2,21 +2,10 @@ package algo.mergesort;
 
 public class App {
     public static void main(String[] args) {
-        int[] myArray = mergeSort(new int[] {7, 9, 3, 5, 11, 1, 4});
+        int[] myArray = new int[] {7, 9, 3, 5, 11, 1, 4};
         MergeSort sorter = new MergeSort();
+
         sorter.sort(myArray);
-
-        displayArray(myArray);
-    }
-
-    public static int[] mergeSort(int[] arr) {
-
-        return arr;
-    }
-
-    public static void displayArray(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(" [" + arr[i] + "]");
-        }
+        sorter.displayArray(myArray);
     }
 }
--- src/algo/mergesort/MergeSort.java
+++ src/algo/mergesort/MergeSort.java
@@ -18,6 +18,53 @@ public class MergeSort {
     }
 
     public static void merge(int[] inputArray, int start, int mid, int end) {
-        
+        int tempArray[] = new int[end - start + 1];
+
+        // index counter for the left side of the array
+        int leftSlot = start;
+        // index counter for the right side of the array
+        int rightSlot = mid + 1;
+        int k = 0;
+
+        while (leftSlot <= mid && rightSlot <= end) {
+            if (inputArray[leftSlot] < inputArray[rightSlot]) {
+                tempArray[k] = inputArray[leftSlot];
+                leftSlot++;
+            }
+            else {
+                tempArray[k] = inputArray[rightSlot];
+                rightSlot++;
+            }
+            k++;
+        }
+
+        /**
+         * At this point, left and right side of sub-array are sorted
+         */
+
+        if (leftSlot <= mid) {
+            while (leftSlot <= mid) {
+                tempArray[k] = inputArray[leftSlot];
+                leftSlot++;
+                k++;
+            }
+        }
+        else if (rightSlot <= end) {
+            while (rightSlot <= end) {
+                tempArray[k] = inputArray[rightSlot];
+                rightSlot++;
+                k++;
+            }
+        }
+
+        // overwrite inputArray with tempArray
+        for (int i = 0; i < tempArray.length; i++)
+            inputArray[start + i] = tempArray[i];
+    }
+
+    public static void displayArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" [" + arr[i] + "]");
+        }
     }
 }
